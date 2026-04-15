519e15749fccaea930b618cace42eead650b3ec7
Princi Rakotoarison <princirakotoarison07@gmail.com>
2026-04-15 21:06:40 +0300

    Initial commit

---
+++ src/main/java/com/mada/visa_mg/VisaMgApplication.java
@@ -0,0 +1,13 @@
+package com.mada.visa_mg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisaMgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisaMgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mada/visa_mg/VisaMgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mada.visa_mg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisaMgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
