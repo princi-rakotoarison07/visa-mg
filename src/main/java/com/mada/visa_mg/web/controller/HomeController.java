9a310296326e751a1f13cf825e13fd7808d6e78f
Princi Rakotoarison <princirakotoarison07@gmail.com>
2026-04-15 22:22:42 +0300

    initialisation  du projet , WEB/API.REST/MVC

---
+++ src/main/java/com/mada/visa_mg/web/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.mada.visa_mg.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "/home"})
+    public String home() {
+        return "index";
+    }
+}
